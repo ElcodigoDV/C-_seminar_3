b7f465c77e7ee33a095a5b34f1faed7f18031628
Kurban Dmitriy V <d_kurban@yahoo.com>
2023-01-24 01:12:42 +1000

    frv

--- ex_23/Program.cs
+++ ex_23/Program.cs
@@ -5,3 +5,23 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+double ToCube (int num)
+{
+    for (int i = 1, i < num+1; i++;)
+    {
+        double cube = Math.Pow(i, 3);
+        Console.Write(cube + ", ");
+    }
+}
+
+int GetNumber(string text)
+{
+    Console.WriteLine("Insert some number");
+    string text = Console.ReadLine();
+    int num = Int32.Parse(text);
+    return num;
+}
+
+GetNumber (text);
+ToCube (num);
