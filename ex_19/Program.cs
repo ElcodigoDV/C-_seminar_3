3d1e312e3c0a45b0318cfcf236835b2e7d56d5c1
Dmitriy <dkurban@yahoo.com>
2023-01-25 00:56:21 +1000

    to be continued

--- ex_19/Program.cs
+++ ex_19/Program.cs
@@ -5,3 +5,30 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+void CheckPal(string str)
+{
+    int n = str.Lenght;
+    for (int i = 0; i < 2; i++)
+    {
+        if (str[i] != str[n-i])
+        {
+            Console.WriteLine("Не палиндром");
+        }
+        
+    }   
+}
+ 
+
+int GetNum(string text)
+{
+    Console.WriteLine(text);
+    string str = Console.ReadLine();
+    // char[] arr = str.ToCharArray();
+    // return arr;
+    return str
+    // Console.WriteLine($"This is 2nd el of array {arr[1]}?");
+}
+
+int arr = GetNum("Введите 5-тизначное число");
+CheckPal(arr);
